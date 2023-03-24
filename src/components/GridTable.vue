1dd210c7fe092235dea083fdd8946ef77aa3279e
kelezyb <hbdev@hongbung.com>
2023-03-24 18:04:42 +0800

    样式优化

--- src/components/GridTable.vue
+++ src/components/GridTable.vue
@@ -1,21 +1,30 @@
 <template>
-  <div class="grid-table" :style="cssVars">
-    <div class="grid-table-tr">
-      <div class="grid-table-th" v-for="(column, index) in columns" :key="index"> {{ column.label }}</div>
-    </div>
+  <div class="grid-table-container">
+    <div class="grid-table" :style="cssVars">
+      <div class="grid-table-header">
+        <div class="grid-table-tr">
+          <div class="grid-table-th" v-for="(column, index) in columns" :key="index"> {{ column.label }}</div>
+        </div>
+      </div>
 
-    <div class="grid-table-body">
-      <div :class="{ activeRow: index == selectIndex }" class="grid-table-tr" v-for="(row, index) in rows" :key="index">
+      <div class="grid-table-body">
         <div
-          ref="xx"
-          @click="select(index)"
-          @dblclick="dblClickColumn({ rowIndex: index, colIndex: colIndex })"
-          class="grid-table-td"
-          v-for="(column, colIndex) in columns"
+          :class="{ activeRow: index == selectIndex }"
+          class="grid-table-tr"
+          v-for="(row, index) in rows"
           :key="index"
         >
-          {{ row[column.name] ?? '' }}</div
-        >
+          <div
+            ref="xx"
+            @click="select(index)"
+            @dblclick="dblClickColumn({ rowIndex: index, colIndex: colIndex })"
+            class="grid-table-td"
+            v-for="(column, colIndex) in columns"
+            :key="index"
+          >
+            {{ row[column.name] ?? '' }}</div
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -71,47 +80,66 @@
   $font-size: 11px;
   $font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana,
     sans-serif;
-  .grid-table {
-    display: table;
-    font-size: $font-size;
-    border-collapse: collapse;
-    font-family: $font-family;
-    ::-webkit-scrollbar {
+  .grid-table-container {
+    display: block;
+    max-height: 200px;
+    overflow: auto;
+    width: 100%;
+    position: relative;
+    table-layout: fixed;
+    border: $border;
+
+    &::-webkit-scrollbar {
       width: 5px;
       height: 5px;
     }
 
     /* 滚动槽 */
-    ::-webkit-scrollbar-track {
+    &::-webkit-scrollbar-track {
       border-radius: 10px;
       /* background-color: rgba(216, 216, 216, 0.5); */
     }
 
-    /* 滚动条滑块 */
-    ::-webkit-scrollbar-thumb {
+    &::-webkit-scrollbar-thumb {
       border-radius: 10px;
       background-color: rgb(226, 232, 241);
     }
+  }
+  .grid-table {
+    display: table;
+    width: 100%;
+    font-size: $font-size;
+    // border-collapse: collapse;
+    font-family: $font-family;
+    table-layout: fixed;
 
-    ::selection {
-      background-color: #bee0ff;
-    }
     .activeRow {
       .grid-table-td {
         background-color: #bee0ff71 !important;
       }
     }
     .grid-table-body {
-      height: 200px;
-      overflow: auto;
-      table-layout: fixed;
+      // max-height: 100px;
+      display: table-row-group;
+      position: relative;
+      // top: var(--rowHeight);
+      overflow-y: auto;
+      border-collapse: collapse;
+      // margin-top: var(--rowHeight);
+    }
+    .grid-table-header {
+      display: table-header-group;
+      position: sticky;
+      z-index: 1;
+      top: 0;
     }
     .grid-table-tr {
       display: table-row;
 
       .grid-table-th {
         user-select: none;
-        border: $border;
+
+        border-bottom: $border;
         display: table-cell;
         height: var(--rowHeight);
         line-height: var(--rowHeight);
@@ -126,8 +154,9 @@
 
       .grid-table-td {
         user-select: auto;
-        color: #5c5c5c;
-        border: $border;
+        color: #7e7e7e;
+        border-right: $border;
+        border-bottom: $border;
         display: table-cell;
         height: var(--rowHeight);
         line-height: var(--rowHeight);
@@ -136,17 +165,14 @@
         min-width: $cell-min-width;
         text-align: center;
         background-color: #fefefe;
+        overflow: hidden;
+        text-overflow: ellipsis; //溢出用省略号显示
+        white-space: nowrap;
+        &:last-child {
+          border-right: 1px solid #fff !important;
+          border-right: 1px solid #fff !important;
+        }
       }
     }
-  }
-
-  div[contenteditable='true']:empty:before {
-    content: attr(placeholder);
-    color: #bab3af;
-    /*padding: 10px 0;*/
-    -webkit-tap-highlight-color: transparent;
-    -webkit-user-modify: read-write;
-    outline: none;
-    border: none;
   }
 </style>
